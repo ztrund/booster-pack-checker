50740e68566e4a2afe790a9262d36211d085a192
Zachary Trundle <ztrunddev@gmail.com>
2023-01-29 09:28:00 -0600

    Update Booster Function Added
    Created the function that updates the boosters in the sqlite database, and updated gitignore file

--- booster_checker_cli.py
+++ booster_checker_cli.py
@@ -1,5 +1,8 @@
 import base64
 import os.path
+import sqlite3
+import time
+
 import requests
 import rsa
 
@@ -28,7 +31,7 @@ def menu():  # Temporary cli menu until I build gui
 def login():  # Add Error Catching
     if os.path.exists('cookie.txt'):
         print('It looks like you are already logged in.')
-        if input('Press 1 if you want to log out and log in again: ') != '1':
+        if input('Press 1 if you want to log out and log in again (or anything else to keep logged in): ') != '1':
             return True
         os.remove('cookie.txt')
     username = input('Username: ')
@@ -47,11 +50,57 @@ def login():  # Add Error Catching
 
 
 def update_boosters():
-    print('Update Boosters')
+    print('Updating Boosters...')
+    base_url = 'https://steamcommunity.com/market/search/render/?q=&category_753_Game%5B%5D=any' \
+               '&category_753_item_class%5B%5D=tag_item_class_5&appid=753&count=100&norender=1&sort_column=name&start='
+    start = 0
+    with open('cookie.txt', 'r') as cookie_file:
+        cookies = {'steamLoginSecure': cookie_file.read()}
+    conn = sqlite3.connect('booster-packs.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS packs (
+                id INT PRIMARY KEY NOT NULL, 
+                name TEXT, 
+                listings INT, 
+                price INT, 
+                update_timestamp INT);""")
+    while True:
+        response = requests.get(base_url + str(start), cookies=cookies)
+        if response.status_code != 200:
+            print('Error' + str(response.status_code))
+            print(response.headers)
+            print(response.reason)
+            print(response.cookies)
+            print(response.raw)
+            print(response.encoding)
+            print(response.elapsed)
+            break
+        data = response.json()
+        if data['success']:
+            bulk_packs = []
+            for result in data['results']:
+                pack = result['hash_name'].split('-', 1)
+                pack[0] = int(pack[0])
+                pack.append(result['sell_listings'])
+                pack.append(result['sell_price'])
+                pack.append(time.time_ns())
+                bulk_packs.append(pack)
+        else:
+            break
+        cur.executemany("INSERT OR REPLACE INTO packs VALUES(?, ?, ?, ?, ?)", bulk_packs)
+        conn.commit()
+        start += 100
+        print(start)
+        # if start >= 1000:
+        #     break
+        if start >= data['total_count']:
+            break
+        time.sleep(1)
+    conn.close()
 
 
 def update_cards():
-    print('Update Trading Cards')
+    print('Updating Trading Cards...')
 
 
 if __name__ == '__main__':
