3d84b086c684c6cb9893e3b99aa99fab40655533
Zachary Trundle <ztrunddev@gmail.com>
2023-02-01 07:39:09 -0600

    Changed Requests to a Request Session
    I wasn't able to find a way to logout and invalidate the steamLoginSecure cookie using requests that were not part of a session. I rewrote the requests to be part of a singular session that is cached between program runs. The logout function now works and invalidates the steamLoginSecure cookie from being able to be used.

--- booster_checker_cli.py
+++ booster_checker_cli.py
@@ -1,5 +1,5 @@
 import base64
-import os.path
+import pickle
 import sqlite3
 import time
 
@@ -12,7 +12,9 @@ def menu():  # Temporary cli menu until I build gui
         print('1.Login to Steam (Increases Update Rate Significantly)')
         print('2.Update Games List and Booster Pack Prices')
         print('3.Update Trading Card Prices')
-        print('4.Exit')
+        print('4.Check Login Status')
+        print('5.Logout of Steam')
+        print('0.Exit')
         choice = input('Enter Selection: ')
         match choice:
             case '1':
@@ -22,6 +24,10 @@ def menu():  # Temporary cli menu until I build gui
             case '3':
                 update_cards()
             case '4':
+                check_login()
+            case '5':
+                logout()
+            case '0':
                 print('Exiting Program...')
                 break
             case _:
@@ -29,24 +35,26 @@ def menu():  # Temporary cli menu until I build gui
 
 
 def login():  # Add Error Catching
-    if os.path.exists('cookie.txt'):
-        print('It looks like you are already logged in.')
-        if input('Press 1 if you want to log out and log in again (or anything else to keep logged in): ') != '1':
-            return True
-        os.remove('cookie.txt')
     username = input('Username: ')
     password = input('Password: ').encode('utf8')
     two_factor = input('2FA Code: ')
-    response_rsa = requests.post('https://steamcommunity.com/login/getrsakey/',
+    response_rsa = req_sess.post('https://steamcommunity.com/login/getrsakey/',
                                  data={'username': username})
     public_key = rsa.PublicKey(int(response_rsa.json()['publickey_mod'], 16),
                                int(response_rsa.json()['publickey_exp'], 16))
     encrypted_password = base64.b64encode(rsa.encrypt(password, public_key))
-    response_login = requests.post('https://steamcommunity.com/login/dologin/',
+    response_login = req_sess.post('https://steamcommunity.com/login/dologin/',
                                    data={'username': username, 'password': encrypted_password,
                                          'rsatimestamp': response_rsa.json()['timestamp'], 'twofactorcode': two_factor})
-    with open('cookie.txt', 'w') as cookie_file:
-        cookie_file.write(response_login.cookies['steamLoginSecure'])
+    with open('cookies', 'wb') as f:
+        pickle.dump(req_sess.cookies, f)
+    print(response_login.status_code)
+    print(response_login.headers)
+    print(response_login.reason)
+    print(response_login.cookies)
+    print(response_login.raw)
+    print(response_login.encoding)
+    print(response_login.elapsed)
 
 
 def update_boosters():
@@ -54,8 +62,6 @@ def update_boosters():
     base_url = 'https://steamcommunity.com/market/search/render/?q=&category_753_Game%5B%5D=any' \
                '&category_753_item_class%5B%5D=tag_item_class_5&appid=753&count=100&norender=1&sort_column=name&start='
     start = 0
-    with open('cookie.txt', 'r') as cookie_file:
-        cookies = {'steamLoginSecure': cookie_file.read()}
     conn = sqlite3.connect('booster-packs.db')
     cur = conn.cursor()
     cur.execute("""CREATE TABLE IF NOT EXISTS packs (
@@ -65,7 +71,7 @@ def update_boosters():
                 price INT, 
                 update_timestamp INT);""")
     while True:
-        response = requests.get(base_url + str(start), cookies=cookies)
+        response = req_sess.get(base_url + str(start))
         if response.status_code != 200:
             print('Error' + str(response.status_code))
             print(response.headers)
@@ -103,5 +109,42 @@ def update_cards():
     print('Updating Trading Cards...')
 
 
+def check_login():
+    print('Checking Login Status...')
+    response_logged_in = req_sess.get('https://steamcommunity.com/my', allow_redirects=False)
+    print(response_logged_in.status_code)
+    print(response_logged_in.headers)
+    print(response_logged_in.reason)
+    print(response_logged_in.cookies)
+    print(response_logged_in.raw)
+    print(response_logged_in.encoding)
+    print(response_logged_in.elapsed)
+
+
+def logout():
+    print('Logging Out...')
+    response_logout = req_sess.post('https://steamcommunity.com/login/logout/',
+                                    data={'sessionid': req_sess.cookies.get('sessionid')})
+    print(response_logout.status_code)
+    print(response_logout.headers)
+    print(response_logout.reason)
+    print(response_logout.cookies)
+    print(response_logout.raw)
+    print(response_logout.encoding)
+    print(response_logout.content)
+    print(response_logout.elapsed)
+    print(response_logout.history)
+    print(response_logout.url)
+    print(response_logout.links)
+
+
 if __name__ == '__main__':
-    menu()
+    with requests.Session() as req_sess:
+        try:
+            with open('cookies', 'rb') as f:
+                req_sess.cookies.update(pickle.load(f))
+        except IOError:
+            pass
+        menu()
+        with open('cookies', 'wb') as f:
+            pickle.dump(req_sess.cookies, f)
